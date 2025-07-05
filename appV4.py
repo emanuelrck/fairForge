58caa1e4adfa64ea5de8c8660438acbb24d22bad
Emanuel Roque <emanuelroque1@hotmail.com>
2025-07-05 21:23:14 +0100

    d

--- appV4.py
+++ appV4.py
@@ -1326,7 +1326,7 @@ if st.session_state["b1"]:
 
 
             st.sidebar.markdown("""<h2>Attribute Specifications</h2>""", unsafe_allow_html=True)
-            with st.sidebar.expander(" ff", expanded=False):
+            with st.sidebar.expander("""<h2>Attribute Specifications</h2>""", expanded=False):
                     st.session_state["default_target_column"] = st.selectbox(
                         "Choose target column:", 
                         df.columns.tolist(), 
