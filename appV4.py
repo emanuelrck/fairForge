4d3b725598894bc50a98f22abd332b59e42a84bb
Emanuel Roque <emanuelroque1@hotmail.com>
2025-07-06 22:15:47 +0100

    df

--- appV4.py
+++ appV4.py
@@ -73,10 +73,6 @@ def main_frontend():
             padding-left: 10%;
         }
 
-        .st-c4 {
-            background-color: rgb(50 93 121) !important;
-        }
-
     
         .st-cf {
             color: #325D79; !important;
