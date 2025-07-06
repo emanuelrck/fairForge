2c2532e82540ac1a3ca0268f885466165079f74f
Emanuel Roque <emanuelroque1@hotmail.com>
2025-07-06 21:05:14 +0100

    f

--- appV4.py
+++ appV4.py
@@ -55,8 +55,8 @@ def main_frontend():
             gap: 8px;
             transition: background 0.3s, color 0.3s, box-shadow 0.3s;
             box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.1); 
-            margin-top: 5%;
-            /*margin-bottom: 5%;*/
+            margin-top: 2%;
+            margin-bottom: 2%;
         }
 
         .stDownloadButton {
